5aa344e4d32f6ed1dd8c3ba64a125fdc4f81c2cf
ro <mr_vidali@mail.ru>
2024-02-27 03:35:45 +0700

    change

--- mySimpleComputer/sc_commandDecode.c
+++ mySimpleComputer/sc_commandDecode.c
@@ -3,7 +3,7 @@
 int
 sc_commandDecode (int value, int *sign, int *command, int *operand)
 {
-  if (value & ~0xFFFF)
+  if (value & ~0x7FFF)
     return -1; // биты, не соответствующие формату команды
   *sign = (value & COMMAND_SIGN_BIT) ? 1 : 0;
   *command = (value & COMMAND_COMMAND_BITS) >> COMMAND_COMMAND_SHIFT;
